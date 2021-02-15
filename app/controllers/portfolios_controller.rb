ce66e9ff90efe18cde7626fee44e5702149e63c5
Austin McCombs <austinmccombs22@gmail.com>
2021-02-14 20:35:41 -0700

    added security so only site admins can use portfolio drag and drop feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.by_position
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
 
     render nothing: true 
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
