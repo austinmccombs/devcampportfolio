e790318c4c5fccfffae435daf0478eaf3f2781b6
Austin McCombs <austinmccombs22@gmail.com>
2021-02-22 22:52:40 -0700

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,4 +48,19 @@ module ApplicationHelper
     def active? path 
         "active" if current_page? path
     end
+
+
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+        
+        if alert
+            alert_generator alert
+        end
+        
+    end
+
+    def alert_generator msg
+        js add_gritter(msg, 
+            title: 'Austin McCombs Portfolio', sticky: false) 
+    end
 end
