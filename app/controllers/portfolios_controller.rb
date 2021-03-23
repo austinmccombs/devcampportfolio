33f1a1f7e00a2d0740263184af819cc11afcb06d
Austin McCombs <austinmccombs22@gmail.com>
2021-03-23 17:06:23 -0600

    added links to portfolio so user can be linked directly to projects

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -68,6 +68,7 @@ class PortfoliosController < ApplicationController
       params.require(:portfolio).permit(:title, 
                                         :subtitle, 
                                         :body, 
+                                        :link,
                                         :main_image,
                                         :thumb_image,
                                         technologies_attributes: [:id, :name, :_destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210228190305) do
+ActiveRecord::Schema.define(version: 20210323220237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20210228190305) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
+    t.string "link"
   end
 
   create_table "skills", id: :serial, force: :cascade do |t|
